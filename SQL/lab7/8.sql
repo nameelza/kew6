3b3bda0e1deb2c5b484dd44c296d337e2cca5c34
nameelza <82921161+nameelza@users.noreply.github.com>
2021-06-19 16:10:25 -0700

    8

--- SQL/lab7/8.sql
+++ SQL/lab7/8.sql
@@ -1,4 +1,1 @@
-SELECT name FROM songs WHERE artist_id IN (SELECT id FROM artists WHERE name = "Post Malone");
-
-
-SELECT name FROM songs, artists WHERE songs.name = artists.name;
+SELECT name FROM songs WHERE name LIKE "%feat%";
