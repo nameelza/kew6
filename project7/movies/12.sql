9e23d8096e0d6a4319ab091ee03a5c602c1bd3f2
nameelza <82921161+nameelza@users.noreply.github.com>
2021-06-20 14:25:35 -0700

    12

---
+++ project7/movies/12.sql
@@ -0,0 +1,1 @@
+SELECT * FROM (SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp"))) WHERE title IN (SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter")));
