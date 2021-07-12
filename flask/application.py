64ca4846a85577fc1248ed07b530fdfea786c1d5
nameelza <82921161+nameelza@users.noreply.github.com>
2021-07-12 07:49:44 -0700

    flask

--- flask/application.py
+++ flask/application.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name=request.args.get("name"))
+    return render_template("index.html")
+
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
 
