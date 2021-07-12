975e4a4594e30bc948ce74a88624ff1823b8aaed
nameelza <82921161+nameelza@users.noreply.github.com>
2021-07-12 07:09:52 -0700

    flask

--- flask/application.py
+++ flask/application.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from os import name
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", name=request.args.get("name"))
 
