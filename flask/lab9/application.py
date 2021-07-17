1a96e97b1af8b54f5eeb3e392fb5468feb5a07c7
nameelza <82921161+nameelza@users.noreply.github.com>
2021-07-17 14:16:31 -0700

    lab9

--- flask/lab9/application.py
+++ flask/lab9/application.py
@@ -17,8 +17,15 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        
+        # access data from form
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        # insert data into database
+        db.execute("INSERT INTO bdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+
+        # go back to homepage
         return redirect("/")
 
     else:
