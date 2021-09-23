e48bc1ad717d5ddc5e4200bb4cbbbae03f750d9e
nameelza <82921161+nameelza@users.noreply.github.com>
2021-09-23 13:12:56 -0700

    js

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,1 +1,13 @@
-let 
+/* Open when someone clicks on the element */
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let btnopen = document.getElementById("btnopen");
+    btnopen.addEventListener("click", function() {
+        document.getElementById("myNav").style.width = "100%";
+    }); 
+});
+
+  
+  /* Close when someone clicks on the arrows inside the overlay */
