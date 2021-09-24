a580818df9a988ddef0592868e9c232a0f1c7189
nameelza <82921161+nameelza@users.noreply.github.com>
2021-09-24 09:34:26 -0700

    js

--- finalproject/static/index.js
+++ finalproject/static/index.js
@@ -1,3 +1,4 @@
+// Make sidebar icon visible on main page load
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector(".todoIcon").style.visibility = "visible";
   });
--- finalproject/static/script.js
+++ finalproject/static/script.js
@@ -1,3 +1,4 @@
+// Open and close the todolists sidebar onclick
 document.addEventListener('DOMContentLoaded', function() {
   let content = document.getElementById("myNav");
   let buttonopen = document.getElementById("buttonOpen");
