f7cf34ef7c710bb6d8ac9671228b7e86132d1028
nameelza <82921161+nameelza@users.noreply.github.com>
2021-09-30 13:39:16 -0700

    fp

--- finalproject/application.py
+++ finalproject/application.py
@@ -40,7 +40,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = sqlite3.connect("project.db", check_same_thread=False)
+db = sqlite3.connect("project.db")
 
 @app.route("/")
 def index():
