ac547bedeaf5d2046cee4f6f14a86425ded79ad5
nameelza <82921161+nameelza@users.noreply.github.com>
2021-10-04 12:22:26 -0700

    app.py

---
+++ fproject/static/index.js
@@ -0,0 +1,4 @@
+// Make sidebar icon visible on main page load
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector(".todoIcon").style.visibility = "visible";
+  });
