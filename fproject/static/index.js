4e4dad933abd515ccc5af701aac78ef42b586f47
nameelza <82921161+nameelza@users.noreply.github.com>
2021-10-08 15:50:11 -0700

    js timer

--- fproject/static/index.js
+++ fproject/static/index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector(".todoIcon").style.visibility = "visible";
   });
 
+// 
 let start = document.getElementById("start");
 let brk = document.getElementById("break");
 let cntn = document.getElementById("continue");
@@ -11,22 +12,65 @@ let finish = document.getElementById("finish");
 let m = document.getElementById("minutes");
 let s = document.getElementById("seconds");
 
+let time;
+let timerIsOn = 0;
+let t;
+
+
+start.addEventListener("click", function() {
+    if (!timerIsOn) {
+        timerIsOn = 1;
+        t = setInterval(startTimer, 1000);
+        start.style.display = "none";
+        brk.style.display = "block";
+        finish.style.display = "block";
+    }
+});
+
+brk.addEventListener("click", function() {
+    timerIsOn = 0;
+    clearInterval(t);
+    start.style.display = "none";
+    brk.style.display = "none";
+    finish.style.display = "block";
+    cntn.style.display = "block";
+});
+
+cntn.addEventListener("click", function() {
+    timerIsOn = 1;
+    t = setInterval(startTimer, 1000);
+    start.style.display = "none";
+    brk.style.display = "block";
+    finish.style.display = "block";
+    cntn.style.display = "none";
+});
 
 
 
+function startTimer() {
+    time = parseInt(m.value, 10) * 60 + parseInt(s.value, 10);
+    time--;
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
 
-// Timer function accepting minutes ans seconds as arguments
-function timer(minutes, seconds) {
-    var timer = minutes * 60 + seconds;
-    var interval = setInterval(function() {
-        var min = parseInt(timer / 60, 10);
-        var sec = parseInt(timer % 60, 10);
-        min = min < 10 ? "0" + min : min;
-        sec = sec < 10 ? "0" + sec : sec;
-        document.getElementById("timer").innerHTML = min + ":" + sec;
-        if (--timer < 0) {
-            clearInterval(interval);
-            document.getElementById("timer").innerHTML = "00:00";
-        }
-    }, 1000);
+    if (minutes < 10) {
+      m.value = "0" + minutes;
+  } else {
+      m.value = minutes;
+  }
+
+    if (seconds < 10) {
+        s.value = "0" + seconds;
+  } else {
+        s.value = seconds;
+  }
 }
+
+
+
+
+
+
+
+
+
