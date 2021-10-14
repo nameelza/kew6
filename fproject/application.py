37634756a2e31dca5a47ac42421886fb56b2c99f
nameelza <82921161+nameelza@users.noreply.github.com>
2021-10-14 12:30:48 -0700

    account css

--- fproject/application.py
+++ fproject/application.py
@@ -33,8 +33,8 @@ class Users(db.Model):
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for('login', next=request.url))
+        if session.get("user_id") is None:
+            return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
 
